636fc7fa1608b90258bbdae8f8ba69e8dd3df890
elroe <e.roessner@chello.at>
2016-08-02 19:59:15 +0200

    changes

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,10 +1,15 @@
 package controllers;
 
+import com.avaje.ebean.Model;
 import models.Person;
 import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.List;
+
+import static play.libs.Json.toJson;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -26,7 +31,11 @@ public class HomeController extends Controller {
         Person person = Form.form(Person.class).bindFromRequest().get();
         person.save();
         return redirect(routes.HomeController.index());
+    }
 
+    public Result getPersons(){
+        List<Person> persons = new Model.Finder(String.class, Person.class).all();
+        return ok(toJson(persons));
     }
 
 }
